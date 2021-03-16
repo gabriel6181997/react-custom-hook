c3b5a3f1a7e3a91e013d653024d4aaa7cd408ef1
“gabriel6181997” <gabriel6181997@gmail.com>
2021-03-16 14:09:30 +0800

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { UserCard } from "./components/UserCard";
+import { UserAllUsers } from "./hooks/useAllUsers";
+import "./styles.css";
+
+export default function App() {
+  const {getUsers, userProfiles, loading, error} = UserAllUsers();
+
+  const onClickFetchUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
